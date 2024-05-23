8eddaee35dd97b5bee5f869cd51f13066d7cf5cf
TiagoCorsato <pg25tiago@vfs.com>
2024-05-22 21:49:46 -0700

    Implemented Emojis with networking system;

--- Assets/Scripts/Emojis/EmojiController.cs
+++ Assets/Scripts/Emojis/EmojiController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using Photon.Pun;
 
 public class EmojiController : MonoBehaviour
 {
@@ -11,37 +12,61 @@ public class EmojiController : MonoBehaviour
     [SerializeField] private float emojiLifeTime = 5f;
 
     [Header("Player data")]
-    [SerializeField] private int _emojiIndex;
     [SerializeField] private int _userIndex;
     [SerializeField] private string _userName;
+
+    GamePlayManager gamePlayManager;
 
     private void Awake()
     {
         spawnTransform = GameObject.FindGameObjectWithTag("EmojiSpawn").transform;
         canSpawnEmoji = true;
+
+        gamePlayManager = FindAnyObjectByType<GamePlayManager>();
+
+        //PhotonNetwork.LocalPlayer.
     }
 
     public void BTPlayEmoji()
     {
-        SpawnEmoji(_emojiIndex, _userIndex, _userName);
+        SpawnEmoji();
+    }
+    
+    public void SpawnEmoji()               
+    {
+        gamePlayManager = FindAnyObjectByType<GamePlayManager>();
+        if (canSpawnEmoji)
+        {
+            Player player = gamePlayManager.players[PhotonNetwork.LocalPlayer.ActorNumber - 1];
+            if (player.playerIndex == PhotonNetwork.LocalPlayer.ActorNumber - 1)
+            {
+                _userIndex = player.playerIndex;
+                _userName = player.playerName;
+
+                canSpawnEmoji = false;
+                StartCoroutine(EmojiCooldown());
+                // Instatiates an emoji at the player's emoji transform
+                    
+                GameObject emojiInstance = PhotonNetwork.Instantiate(emojiPrefab.name, player.gameObject.transform.position, emojiPrefab.transform.rotation);
+                emojiInstance.transform.SetParent(FindAnyObjectByType<UIManager>().transform);
+                emojiInstance.GetComponentInChildren<Animator>().Play(emojiAnimation);
+
+                // Update emoji username
+                emojiInstance.GetComponentInChildren<TextMeshProUGUI>().text = _userName;
+
+                StartCoroutine(NetworkDestroy(emojiInstance));
+            }
+        }     
     }
 
-    //[PunRPC]
-    public void SpawnEmoji(int emojiIndex, int userIndex, string userName)               
+    IEnumerator NetworkDestroy(GameObject objectToDestroy)
     {
-        if(canSpawnEmoji)
+        yield return new WaitForSeconds(emojiLifeTime);
+
+        if (objectToDestroy.GetComponent<PhotonView>().IsMine)
         {
-            canSpawnEmoji = false;
-            StartCoroutine(EmojiCooldown());
-            // Instatiates an emoji at the player's emoji transform
-            GameObject emojiInstance = Instantiate(emojiPrefab, spawnTransform);
-            emojiInstance.GetComponentInChildren<Animator>().Play(emojiAnimation);
-
-            // Update emoji username
-            GetComponentInChildren<TextMeshProUGUI>().text = userName;
-
-            Destroy(emojiInstance, emojiLifeTime);
-        }     
+            PhotonNetwork.Destroy(objectToDestroy);
+        }
     }
 
     IEnumerator EmojiCooldown()
