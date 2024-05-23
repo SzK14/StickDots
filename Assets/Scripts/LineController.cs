1b7ce2e31ac0f2946363bcf48d0db16120831bf7
pg25kawai <144267324+pg25kawai@users.noreply.github.com>
2024-05-22 20:29:06 -0700

    Comment out PUN code to make local mode work

--- Assets/Scripts/LineController.cs
+++ Assets/Scripts/LineController.cs
@@ -122,12 +122,12 @@ public class LineController : MonoBehaviour
                 if (_p1.y == _p2.y && Mathf.Abs(_p1.x - _p2.x) == 1 ||
                     _p1.x == _p2.x && Mathf.Abs(_p1.y - _p2.y) == 1)
                 {
-                    //MakeLine(_dotPositions[0], _dotPositions[1]);
-                    photonView.RPC("MakeLine", RpcTarget.All, (Vector2)_dotPositions[0], (Vector2)_dotPositions[1]);
+                    MakeLine(_dotPositions[0], _dotPositions[1]);
+                    //photonView.RPC("MakeLine", RpcTarget.All, (Vector2)_dotPositions[0], (Vector2)_dotPositions[1]);
 
                     Debug.Log($"Player{GamePlayManager.Instance.currentPlayerIndex}: {_p1}, {_p2}");
-                    //GamePlayManager.Instance.PlayersMove(_p1, _p2);
-                    GamePlayManager.Instance.PlayersMoveRPC(_p1, _p2);
+                    GamePlayManager.Instance.PlayersMove(_p1, _p2);
+                    //GamePlayManager.Instance.PlayersMoveRPC(_p1, _p2);
 
                     //hide lineDrawable if creating new line
                     _LineDrawable.SetActive(false);
