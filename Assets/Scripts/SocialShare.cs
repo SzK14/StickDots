00d3dbe9180997dc3717f6b0ca2eb14f2b0afa41
MichaelLongKitLam <pg25michael@vfs.com>
2024-05-22 22:21:24 -0700

    build success, social sharing working

--- Assets/Scripts/SocialShare.cs
+++ Assets/Scripts/SocialShare.cs
@@ -1,23 +1,29 @@
 using UnityEngine;
 using VoxelBusters.EssentialKit;
+using VoxelBusters.CoreLibrary;
 
 public class SocialShare : MonoBehaviour
 {
     [SerializeField] public string shareText = "";
-    private ShareSheet _shareSheet;
+    [SerializeField] private ShareSheet _shareSheet = null;
 
     //ont start create instance
     private void Start()
     {
-        _shareSheet = ShareSheet.CreateInstance();
+    }
+
+    private void OnShareSheetClosed(ShareSheetResult result, Error error)
+    { 
+        // reset instance
+        _shareSheet = null;
     }
 
     //call on game end to automatically take screen shot
     public void TakeScreeShot()
     {
-        Debug.Log("add text and screenshot");
         if (_shareSheet != null) 
         {
+            Debug.Log("add text and screenshot");
             _shareSheet.AddText(shareText);
             _shareSheet.AddScreenshot();
         }
@@ -26,8 +32,8 @@ public class SocialShare : MonoBehaviour
     //call when share button is clicked
     public void Share()
     {
-        Debug.Log("share");
         if (_shareSheet != null) { 
+            Debug.Log("share");
             _shareSheet.Show();
         }
     }
@@ -35,6 +41,9 @@ public class SocialShare : MonoBehaviour
     //Test Button function
     public void Test()
     {
+        var newSheet = ShareSheet.CreateInstance();
+        newSheet.SetCompletionCallback(OnShareSheetClosed);
+        _shareSheet = newSheet;
         TakeScreeShot();
         Share();
     }
